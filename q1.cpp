ade3cfa1934bd6c1ad4bf6fe2177112d25b7d169
EE211315 <ee211315@pcz-ee211315.sasken.com>
2019-11-11 11:56:14 +0530

    message

---
+++ q1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Base
+ {
+   public:
+   void f() { 
+      cout<<"Base\n";
+   }
+};
+class Derived:public Base {
+public: 
+   void f() {
+      cout<<"Derived\n";
+   };
+};
+main() { 
+   Derived obj; 
+   obj.Base::f();
+}
